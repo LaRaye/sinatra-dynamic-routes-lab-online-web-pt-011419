5d7eb728920d650a42bc2f3fb57df4c72c9a25ab
LaRaye Johnson <lnj2105@gmail.com>
2019-04-27 21:04:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
   # Write your code here!
+  
+  get '/reversename/:name' do 
+    @name = params[:name]
+    @name.reverse.to_s
+  end 
 
+  get '/square/:number' do 
+    @number = params[:number]
+    "#{@number.to_i**2}"
+  end 
+
+  get '/say/:number/:phrase' do 
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    
+    #binding.pry 
+    erb :say 
+  end 
 end
